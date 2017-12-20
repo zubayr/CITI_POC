1ab27b7d8df6b26d8fe1a9f05138ea711b0f1066
zubayr <34240777+zubayr@users.noreply.github.com>
2017-12-20 12:04:39 +0530

    Update BaseController.java

--- src/main/java/com/mkyong/controller/BaseController.java
+++ src/main/java/com/mkyong/controller/BaseController.java
@@ -17,7 +17,7 @@ public class BaseController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String welcome(ModelMap model) {
 
-		model.addAttribute("message", "Welcome to INFOSYS");
+		model.addAttribute("message", "Welcome To INFOSYS LTD.");
 		model.addAttribute("counter", ++counter);
 		logger.debug("[welcome] counter : {}", counter);
 
